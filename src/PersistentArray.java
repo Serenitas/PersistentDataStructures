89f29ef953ed2b43f08560106557379f378d7826
Исаченко Владимир <vv.isachenko@gmail.com>
2017-12-21 23:14:18 +0700

    Added first realization of persistent array

--- src/PersistentArray.java
+++ src/PersistentArray.java
@@ -6,212 +6,32 @@ import java.util.function.UnaryOperator;
 /**
  * Created by Svetlana on 21.12.2017.
  */
-public class PersistentArray extends ArrayList
-{
-    public PersistentArray(int initialCapacity)
-    {
-        super(initialCapacity);
+public class PersistentArray <E> {
+    private int currentVersion = 0;
+    private ArrayList <TreeMap <Integer, E>> versionedData;
+
+    public PersistentArray(int minCapacity) {
+        versionedData = new ArrayList<>(minCapacity);
+        for (int i = 0; i < minCapacity; i++) {
+            versionedData.set(i, new TreeMap<Integer, E>());
+            versionedData.get(i).put(currentVersion, null);
+        }
     }
 
-    public PersistentArray()
-    {
-        super();
+    public E get(int index, int version) {
+        return versionedData.get(index).floorEntry(version).getValue();
     }
 
-    public PersistentArray(Collection c)
-    {
-        super(c);
+    public E get(int index) {
+        return get(index, currentVersion);
     }
 
-    @Override
-    public void trimToSize()
-    {
-        super.trimToSize();
+    public void set(int index, E obj) {
+        currentVersion++;
+        versionedData.get(index).put(currentVersion, obj);
     }
 
-    @Override
-    public void ensureCapacity(int minCapacity)
-    {
-        super.ensureCapacity(minCapacity);
-    }
-
-    @Override
-    public int size()
-    {
-        return super.size();
-    }
-
-    @Override
-    public boolean isEmpty()
-    {
-        return super.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o)
-    {
-        return super.contains(o);
-    }
-
-    @Override
-    public int indexOf(Object o)
-    {
-        return super.indexOf(o);
-    }
-
-    @Override
-    public int lastIndexOf(Object o)
-    {
-        return super.lastIndexOf(o);
-    }
-
-    @Override
-    public Object clone()
-    {
-        return super.clone();
-    }
-
-    @Override
-    public Object[] toArray()
-    {
-        return super.toArray();
-    }
-
-    @Override
-    public Object[] toArray(Object[] a)
-    {
-        return super.toArray(a);
-    }
-
-    @Override
-    public Object get(int index)
-    {
-        return super.get(index);
-    }
-
-    @Override
-    public Object set(int index, Object element)
-    {
-        return super.set(index, element);
-    }
-
-    @Override
-    public boolean add(Object o)
-    {
-        return super.add(o);
-    }
-
-    @Override
-    public void add(int index, Object element)
-    {
-        super.add(index, element);
-    }
-
-    @Override
-    public Object remove(int index)
-    {
-        return super.remove(index);
-    }
-
-    @Override
-    public boolean remove(Object o)
-    {
-        return super.remove(o);
-    }
-
-    @Override
-    public boolean containsAll(Collection c)
-    {
-        return super.containsAll(c);
-    }
-
-    @Override
-    public void clear()
-    {
-        super.clear();
-    }
-
-    @Override
-    public boolean addAll(Collection c)
-    {
-        return super.addAll(c);
-    }
-
-    @Override
-    public boolean addAll(int index, Collection c)
-    {
-        return super.addAll(index, c);
-    }
-
-    @Override
-    protected void removeRange(int fromIndex, int toIndex)
-    {
-        super.removeRange(fromIndex, toIndex);
-    }
-
-    @Override
-    public boolean removeAll(Collection c)
-    {
-        return super.removeAll(c);
-    }
-
-    @Override
-    public boolean retainAll(Collection c)
-    {
-        return super.retainAll(c);
-    }
-
-    @Override
-    public ListIterator listIterator(int index)
-    {
-        return super.listIterator(index);
-    }
-
-    @Override
-    public ListIterator listIterator()
-    {
-        return super.listIterator();
-    }
-
-    @Override
-    public Iterator iterator()
-    {
-        return super.iterator();
-    }
-
-    @Override
-    public List subList(int fromIndex, int toIndex)
-    {
-        return super.subList(fromIndex, toIndex);
-    }
-
-    @Override
-    public void forEach(Consumer action)
-    {
-        super.forEach(action);
-    }
-
-    @Override
-    public Spliterator spliterator()
-    {
-        return super.spliterator();
-    }
-
-    @Override
-    public boolean removeIf(Predicate filter)
-    {
-        return super.removeIf(filter);
-    }
-
-    @Override
-    public void replaceAll(UnaryOperator operator)
-    {
-        super.replaceAll(operator);
-    }
-
-    @Override
-    public void sort(Comparator c)
-    {
-        super.sort(c);
+    public int getLength() {
+        return versionedData.size();
     }
 }
